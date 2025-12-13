d59e6e8ba9bfda5ee51881f00e947db649e3d9df
Connor Wheeler <Connorthevaliant@gmail.com>
2025-12-12 17:38:49 -0700

    Player Gravity

--- starship-slinger/Scripts/player_movement.gd
+++ starship-slinger/Scripts/player_movement.gd
@@ -6,6 +6,9 @@ var fuelTotal = 1000000000
 var fuelLoss = 5
 var inputVec : Vector2
 
+var gravityHomeList: Array[Vector2]
+var gravityStrList: Array[float]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -25,8 +28,23 @@ func _physics_process(delta: float) -> void:
 		self.velocity += inputVec.rotated(rotation) * shipAccel
 		fuelTotal -= fuelLoss
 	
-	#Space friction
-	if inputVec.y == 0:
+	#Space friction and gravity from planets
+	
+	var gravityEffect := Vector2.ZERO
+	for g in gravityStrList.size():
+		var curGravStr = gravityStrList[g]
+		var curGravPos = gravityHomeList[g]
+		var curGravVec = (curGravPos - self.position).normalized()
+		gravityEffect += curGravVec * curGravStr
+	velocity = velocity + gravityEffect
+	
+	if inputVec.y == 0 && gravityEffect != Vector2.ZERO:
 		velocity = velocity.move_toward(Vector2.ZERO, 4)
 	
 	move_and_slide()
+
+
+func _on_gravity_area_area_entered(area: Area2D) -> void:
+	if area.name == "GravityArea":
+		gravityStrList.append(area.get_parent().gravityStr)
+		gravityHomeList.append(area.get_parent().position)
