a1b9a2beb0930da5d67e1d0df44fd0f4e4732b75
Connor Wheeler <Connorthevaliant@gmail.com>
2025-12-21 10:44:07 -0700

    +3 levels

--- starship-slinger/Scripts/main_menu.gd
+++ starship-slinger/Scripts/main_menu.gd
@@ -6,7 +6,7 @@ var playScene
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Highscore.text = 'Highscore: ' + str(GameManager.highScore)
-	playScene = GameManager.allLevels[6]
+	playScene = GameManager.allLevels[8]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
