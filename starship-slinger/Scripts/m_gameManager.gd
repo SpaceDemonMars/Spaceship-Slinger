27c26b01fd55c35a2f04e90719468d6e14acefa9
Connor Wheeler <Connorthevaliant@gmail.com>
2025-12-21 12:06:18 -0700

    level dev

--- starship-slinger/Scripts/m_gameManager.gd
+++ starship-slinger/Scripts/m_gameManager.gd
@@ -40,13 +40,14 @@ var bestScore: int = defaultSaveData["Best Score"] #best level score
 var selectedLevel : PackedScene
 var activeLevel #nodes only
 #IF I AM ASLEEP WHEN YOU MERGE YOUR LEVELS, UNCOMMENT THE FILEPATH BELOW
-var levelsFolder := "res://TESTINGLEVELS/"#"res://Levels/"
+var levelsFolder := "res://Levels/"
 var selectedLevelIndex : int = defaultSaveData["Selected Level Index"]
 var allLevels : Array[PackedScene] = []
 var unlockedLevelIndex: int = defaultSaveData["Unlocked Level Index"]
 
 
 func _ready():
+	saveGame()
 	loadGame()
 	#load level
 	selectedLevel = allLevels[selectedLevelIndex]
